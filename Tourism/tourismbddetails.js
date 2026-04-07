675aa4421f7025918e7c16a7e7d4c6c81f1ca048
TheRudraBro <rudramraj22@gmail.com>
2026-04-07 23:16:08 +0600

    Updated

--- Tourism/tourismbddetails.js
+++ Tourism/tourismbddetails.js
@@ -1,5 +1,10 @@
-function openModal(title, description){
-  document.getElementById("modalTitle").innerText = title;
-  document.getElementById("modalDescription").innerText = description;
-  document.getElementById("detailsModal").showModal();
-}
+  function openModal(title, description, price, duration, spot, highlights) {
+    document.getElementById("modalTitle").innerText = title;
+    document.getElementById("modalDescription").innerText = description;
+    document.getElementById("modalPrice").innerText = price;
+    document.getElementById("modalDuration").innerText = duration;
+    document.getElementById("modalSpot").innerText = spot;
+    document.getElementById("modalHighlights").innerText = highlights;
+
+    document.getElementById("detailsModal").showModal();
+  }
